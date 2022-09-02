f73bea34297687f2f5d15c1f8b9bb9c02eacbe02
MichMav <michmav28@gmail.com>
2022-09-02 18:32:18 +0200

    add the forms for the subscribe

---
+++ forms/registre.blade.php
@@ -0,0 +1,123 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    
+        <title>Bibliothèque en ligne - numérique</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+    
+        <!-- Favicons -->
+        <link href="../assets/img/favicon.ico" rel="icon">
+        <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    
+        <!-- Vendor CSS Files -->
+        <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+        <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+        <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+        <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+        <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+        <!-- Font-awesome File -->
+        <link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
+        
+        <!-- Template Main CSS File -->
+        <link href="../assets/css/style.css" rel="stylesheet">
+        
+        <!-- the style customers -->
+        <link rel="stylesheet" href="../assets/scss/main.css">
+    
+        <!-- =======================================================
+        * Template Name: Flattern - v4.8.0
+        * Template URL: https://bootstrapmade.com/flattern-multipurpose-bootstrap-template/
+        * Author: BootstrapMade.com
+        * License: https://bootstrapmade.com/license/
+        ======================================================== -->
+    </head>
+
+    <body>
+        
+        <div class="grid-container-fluid">
+            <div class="container">
+                <label class="label" for="" data-aos="fade-in" >S'inscrire</label>
+                <form action="" data-aos="fade-up">
+                    <div class="form-group">
+                        <label for="">
+                            <input type="text" class="name form-control is-valid" name="" id="inputName" aria-describedby="helpId" placeholder="Nom">
+                            <div class="invalid-feedback">
+                                Validation message
+                            </div>
+                        </label>
+                        <label for="">
+                            <input type="text" class="name form-control is-valid" name="" id="inputName" aria-describedby="helpId" placeholder="Post-nom">
+                            <div class="invalid-feedback">
+                                Validation message
+                            </div>
+                            
+                        </label>
+                    </div>
+                    <div class="form-group">
+                        <label for=""></label>
+                        <textarea class="adr form-control is-valid" name="" id="inputName" rows="3" aria-describedby="helpId" placeholder="Adresse"></textarea>
+                    </div>
+                    <div class="invalid-feedback">
+                        Validation message
+                    </div><br>
+                    <div class="form-group">
+                        <label for="">
+                            <input type="text" class="name form-control is-valid" name="" id="inputName" aria-describedby="helpId" placeholder="Prénom">
+                            <div class="invalid-feedback">
+                                Validation message
+                            </div>
+                        </label>
+                        <label for="">
+                            <select class="name form-control" name="" id="">
+                                <option>Homme</option>
+                                <option>Femme</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div class="form-group">
+                        <label for=""></label>
+                        <input type="date" class="date form-control is-valid" name="" id="inputName" aria-describedby="helpId" placeholder="Date de naissance">
+                        <div class="invalid-feedback">
+                            Validation message
+                        </div>
+                    </div>
+                    <div class="form-group"><br><br>
+                        <button type="submit" class="btn  btn-success ">Envoyer</button>
+                        <button type="reset" class="btn  btn-danger ">Annuler</button>
+                    </div>
+                </form>
+
+            </div>
+            <div class="other" data-aos="fade-in">
+                <label for="" class="Vous">Vous Pouvez vous connectez</label><br>
+                <a href="#" class="badge badge-primary">
+                 <i class="fa fa-google icon"></i> avec votre compter Google
+                </a>
+            </div>
+            
+        </div>
+
+
+        <!-- Vendor JS Files -->
+        <script src="../assets/vendor/aos/aos.js"></script>
+        <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="../assets/vendor/waypoints/noframework.waypoints.js"></script>
+        <script src="../assets/vendor/php-email-form/validate.js"></script>
+
+        <!-- Template Main JS File -->
+        <script src="../assets/js/main.js"></script>
+    </body>
+</html>
