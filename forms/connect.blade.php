98a3d6e5ac6d7ab6a7488574d87110548389704c
MichMav <michmav28@gmail.com>
2022-09-17 17:29:43 +0200

    add the form for signin

--- forms/connect.blade.php
+++ forms/connect.blade.php
@@ -1,61 +1,80 @@
 <!DOCTYPE html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    
-        <title>Bibliothèque en ligne - numérique</title>
-        <meta content="" name="description">
-        <meta content="" name="keywords">
-    
-        <!-- Favicons -->
-        <link href="../assets/img/favicon.ico" rel="icon">
-        <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-    
-        <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-    
-        <!-- Vendor CSS Files -->
-        <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-        <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
-        <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-        <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-        <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-        <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-        <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-        <!-- Font-awesome File -->
-        <link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
-        
-        <!-- Template Main CSS File -->
-        <link href="../assets/css/style.css" rel="stylesheet">
-        
-        <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css">
+    <title>Bibliothèque en ligne - numérique</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-        <!-- the style customers -->
-        <link rel="stylesheet" href="../assets/scss/main.css">
+    <!-- Favicons -->
+    <link href="../assets/img/favicon.ico" rel="icon">
+    <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Font-awesome File -->
+    <link rel="stylesheet" href="../assets/font-awesome/css/font-awesome.min.css">
     
-    </head>
-    <body class="body2">
-      
-      <div class="container-fluid">
-        <form action="" class="form">
-          <input type="text">
+    <!-- Template Main CSS File -->
+    <link href="../assets/css/style.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css">
+
+    <!-- the style customers -->
+    <link rel="stylesheet" href="../assets/css/style1.Css">
+
+  </head>
+  <body>
+    <div class="box" data-aos="fade-up">
+      <a href="../assets/Views/index.blade.php" class="links-home">
+        <img src="../assets/img/home.png" alt="" class="img-home">
+      </a>
+      <div class="form">
+        <h2>S'identifier</h2>
+        <form action="" method="">
+          <div class="inputBox">
+            <input type="text" name="" id="" required="required">
+            <span>Nom d'utilisateur</span>
+            <i></i>
+          </div>
+          <div class="inputBox">
+            <input type="password" name="" id="" required="required" maxlength="8">
+            <span>Mot de passe</span>
+            <i></i>
+          </div>
+          <div class="links">
+            <a href="#" title="Mot de passe oublié">Mot de passe oublié</a>
+            <a href="registre.blade.php" title="S'inscrire">S'inscrire</a>
+          </div>
+          <input type="submit" value="Connexion" class="submit">
         </form>
       </div>
+    </div>
 
 
-        <script src="../assets/vendor/aos/aos.js"></script>
-        <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
-        <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-        <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
-        <script src="../assets/vendor/waypoints/noframework.waypoints.js"></script>
-        <script src="../assets/vendor/php-email-form/validate.js"></script>
+    <script src="../assets/vendor/aos/aos.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="../assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="../assets/vendor/waypoints/noframework.waypoints.js"></script>
+    <script src="../assets/vendor/php-email-form/validate.js"></script>
 
-        <!-- Template Main JS File -->
-        <script src="../assets/js/main.js"></script>
-        <script src="../assets/bootstrap/js/bootstrap.js"></script>
-    </body>
+    <!-- Template Main JS File -->
+    <script src="../assets/js/main.js"></script>
+    <script src="../assets/bootstrap/js/bootstrap.js"></script>
+  </body>
 </html>
