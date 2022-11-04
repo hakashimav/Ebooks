68c619d5109066f362ca4bd6d7c47d59da3fc346
MichMav <michmav28@gmail.com>
2022-11-04 15:02:23 +0100

    fixe the rechercher

--- assets/Views/pages/blog.blade.php
+++ assets/Views/pages/blog.blade.php
@@ -1,3 +1,21 @@
+<?php 
+  @$keywords = $_GET["keywords"];
+  @$valider = $_GET["valider"];
+
+  if(isset($valider) && !empty(trim($keywords))) {
+    $words = explode(" ", trim($keywords));
+    for($i=0;$i<count($words);$i++)
+      $kw[$i] = "Nomouv like '%".$words[$i]."%'";
+    include ("../../app/connexion.php");
+    $res = $con->prepare("SELECT * FROM ouvrage WHERE ".implode(" or ",$kw));
+    $res->setFetchMode(PDO::FETCH_ASSOC);
+    $res->execute();
+    $tab = $res->fetchAll();
+    $afficher = "oui";
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -39,11 +57,11 @@
         <div class="container" data-aos="fade-up">
 
           <div class="row">
-
+            <?php if (@$afficher == "oui") { ?>
             <div class="col-lg-8 entries">
-
+              <h2 class="entry-title"><?= count($tab). " ".(count($tab)>1?"résultats trouvés":"résultat trouvé")?></h2>
+              <?php for ($i=0;$i<count($tab);$i++) {?>
               <article class="entry">
-
                 <div class="entry-img">
                   <img src="../../img/blog/blog-1.jpg" alt="" class="img-fluid">
                 </div>
@@ -61,107 +79,15 @@
                 </div>
 
                 <div class="entry-content">
-                  <p>
-                    Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
-                    Et eveniet enim. Qui velit est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore.
-                  </p>
+                  <p> <?php echo $tab[$i]["Nomouv"] ?></p>
                   <div class="read-more">
                     <a href="blog-single.blade.php">Read More</a>
                   </div>
                 </div>
 
               </article><!-- End blog entry -->
-
-              <article class="entry">
-
-                <div class="entry-img">
-                  <img src="../../img/blog/blog-2.jpg" alt="" class="img-fluid">
-                </div>
-
-                <h2 class="entry-title">
-                  <a href="blog-single.blade.php">Nisi magni odit consequatur autem nulla dolorem</a>
-                </h2>
-
-                <div class="entry-meta">
-                  <ul>
-                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-single.blade.php">John Doe</a></li>
-                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.blade.php"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-                    <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="blog-single.blade.php">12 Comments</a></li>
-                  </ul>
-                </div>
-
-                <div class="entry-content">
-                  <p>
-                    Incidunt voluptate sit temporibus aperiam. Quia vitae aut sint ullam quis illum voluptatum et. Quo libero rerum voluptatem pariatur nam.
-                    Ad impedit qui officiis est in non aliquid veniam laborum. Id ipsum qui aut. Sit aliquam et quia molestias laboriosam. Tempora nam odit omnis eum corrupti qui aliquid excepturi molestiae. Facilis et sint quos sed voluptas. Maxime sed tempore enim omnis non alias odio quos distinctio.
-                  </p>
-                  <div class="read-more">
-                    <a href="blog-single.blade.php">Read More</a>
-                  </div>
-                </div>
-
-              </article><!-- End blog entry -->
-
-              <article class="entry">
-
-                <div class="entry-img">
-                  <img src="../../img/blog/blog-3.jpg" alt="" class="img-fluid">
-                </div>
-
-                <h2 class="entry-title">
-                  <a href="blog-single.blade.php">Possimus soluta ut id suscipit ea ut. In quo quia et soluta libero sit sint.</a>
-                </h2>
-
-                <div class="entry-meta">
-                  <ul>
-                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-single.blade.php">John Doe</a></li>
-                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.blade.php"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-                    <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="blog-single.blade.php">12 Comments</a></li>
-                  </ul>
-                </div>
-
-                <div class="entry-content">
-                  <p>
-                    Aut iste neque ut illum qui perspiciatis similique recusandae non. Fugit autem dolorem labore omnis et. Eum temporibus fugiat voluptate enim tenetur sunt omnis.
-                    Doloremque est saepe laborum aut. Ipsa cupiditate ex harum at recusandae nesciunt. Ut dolores velit.
-                  </p>
-                  <div class="read-more">
-                    <a href="blog-single.blade.php">Read More</a>
-                  </div>
-                </div>
-
-              </article><!-- End blog entry -->
-
-              <article class="entry">
-
-                <div class="entry-img">
-                  <img src="../../img/blog/blog-4.jpg" alt="" class="img-fluid">
-                </div>
-
-                <h2 class="entry-title">
-                  <a href="blog-single.blade.php">Non rem rerum nam cum quo minus. Dolor distinctio deleniti explicabo eius exercitationem.</a>
-                </h2>
-
-                <div class="entry-meta">
-                  <ul>
-                    <li class="d-flex align-items-center"><i class="bi bi-person"></i> <a href="blog-single.blade.php">John Doe</a></li>
-                    <li class="d-flex align-items-center"><i class="bi bi-clock"></i> <a href="blog-single.blade.php"><time datetime="2020-01-01">Jan 1, 2020</time></a></li>
-                    <li class="d-flex align-items-center"><i class="bi bi-chat-dots"></i> <a href="blog-single.blade.php">12 Comments</a></li>
-                  </ul>
-                </div>
-
-                <div class="entry-content">
-                  <p>
-                    Aspernatur rerum perferendis et sint. Voluptates cupiditate voluptas atque quae. Rem veritatis rerum enim et autem. Saepe atque cum eligendi eaque iste omnis a qui.
-                    Quia sed sunt. Ea asperiores expedita et et delectus voluptates rerum. Id saepe ut itaque quod qui voluptas nobis porro rerum. Quam quia nesciunt qui aut est non omnis. Inventore occaecati et quaerat magni itaque nam voluptas. Voluptatem ducimus sint id earum ut nesciunt sed corrupti nemo.
-                  </p>
-                  <div class="read-more">
-                    <a href="blog-single.blade.php">Read More</a>
-                  </div>
-                </div>
-
-              </article><!-- End blog entry -->
-
+              <?php } ?>
+              
               <div class="blog-pagination">
                 <ul class="justify-content-center">
                   <li><a href="#">1</a></li>
@@ -171,16 +97,16 @@
               </div>
 
             </div><!-- End blog entries list -->
-
+            <?php } ?>
             <div class="col-lg-4">
 
               <div class="sidebar">
 
                 <h3 class="sidebar-title">Search</h3>
                 <div class="sidebar-item search-form">
-                  <form action="" method="">
-                    <input type="text">
-                    <button type="submit"><i class="bi bi-search"></i></button>
+                  <form action="" method="GET">
+                    <input type="text" name="keywords" value="<?= $keywords?>" placeholder="Recherche">
+                    <button type="submit" name="valider"><i class="bi bi-search"></i></button>
                   </form>
                 </div><!-- End sidebar search formn-->
 
