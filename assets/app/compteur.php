3dd9bbca0aa11c87e81b2d003043c92f1c24c9c9
MichMav <michmav28@gmail.com>
2022-11-07 20:52:44 +0100

    fixe the competri 1

--- assets/app/compteur.php
+++ assets/app/compteur.php
@@ -23,42 +23,44 @@ if ( $_SESSION["User"]) {
     $querS->execute();
 
     $lect = $querS->fetch();
-    
+
     if ($lect == true) {
         $num = $lect["Numlecteur"];
-    }
-    $sqlS = "SELECT * FROM compteur WHERE Numlecteur = '$num'";
-    $queryS = $con->prepare($sqlS);
-    $queryS->execute();
-    $fetch = $queryS->fetch();
-    if($fetch == true) {
-        $comp = $fetch["Idcompt"];
-        $Val = $fetch["Validite"];
+    
+        $sqlS = "SELECT * FROM compteur WHERE Numlecteur = '$num'";
+        $queryS = $con->prepare($sqlS);
+        $queryS->execute();
+        $fetch = $queryS->fetch();
+    
+        if($fetch == true) {
+            $comp = $fetch["Idcompt"];
+            $Val = $fetch["Validite"];
 
-        $res = $Val - $hours;
+            $res = $Val - $hours;
 
-        if($res > 0) {
-        $insertS = "UPDATE compteur SET Validite = '$res' WHERE Idcompt = '$comp'";
-        $quer1S = $con->prepare($insertS);
-        $quer1S->execute();
-        }
+            if($res > 0) {
+            $insertS = "UPDATE compteur SET Validite = '$res' WHERE Idcompt = '$comp'";
+            $quer1S = $con->prepare($insertS);
+            $quer1S->execute();
+            }
 
-        $sql1S = "SELECT * FROM compteur WHERE Numlecteur = '$num'";
-        $query1S = $con->prepare($sql1S);
-        $query1S->execute();
-        $fetch1 = $query1S->fetch();
-        if($fetch1 == true){
-        $Val1 = $fetch1["Validite"];
-        }
+            $sql1S = "SELECT * FROM compteur WHERE Numlecteur = '$num'";
+            $query1S = $con->prepare($sql1S);
+            $query1S->execute();
+            $fetch1 = $query1S->fetch();
+            if($fetch1 == true){
+            $Val1 = $fetch1["Validite"];
+            }
 
-        if ($Val1 == 0) {
+            if ($Val1 == 0) {
 
-            $del = " DELETE FROM abonnement WHERE Numlecteur ='$num'";
-            $ok = $con->prepare($del);
-            $ok->execute();
-            $sup = "DELETE FROM compteur WHERE Numlecteur = '$num'" ;
-            $va= $con->prepare($sup);
-            $va->execute();
+                $del = " DELETE FROM abonnement WHERE Numlecteur ='$num'";
+                $ok = $con->prepare($del);
+                $ok->execute();
+                $sup = "DELETE FROM compteur WHERE Numlecteur = '$num'" ;
+                $va= $con->prepare($sup);
+                $va->execute();
+            }
         }
     }
 }
